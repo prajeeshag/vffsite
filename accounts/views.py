26f979beb5cc11c4bbbdee7a976b1e167934a711
deepak <prajeeshag@gmail.com>
2020-07-25 13:39:24 +0530

    templates

--- accounts/views.py
+++ accounts/views.py
@@ -10,34 +10,38 @@ from .forms import signupForm, loginForm
 def home(request):
     return render(request, 'registration/home.html')
 
-def login(request):
+
+def signin(request):
     if request.method == 'POST':
         if settings.DEBUG:
             print(request.POST)
-        if 'login' in request.POST:
-            loginform = loginForm(None, request.POST)
-            signupform = signupForm(tabActive=False)
-            if loginform.is_valid():
-                username = loginform.cleaned_data.get('username')
-                raw_password = loginform.cleaned_data.get('password')
-                user = authenticate(username=username,password=raw_password)
-                login(request,user)
-                return redirect('accounts:home')
+        form = loginForm(None, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=raw_password)
+            login(request,user)
+            return redirect('accounts:home')
 
-        if 'signup' in request.POST:
-            signupform = signupForm(request.POST)
-            loginform = loginForm(tabActive=False)
-            if signupform.is_valid():
-                user=signupform.save()
-                # username = signupform.cleaned_data.get('username')
-                # raw_password = signupform.cleaned_data.get('password1')
-                # user = authenticate(username=username,password=raw_password)
-                login(request,user)
-                return redirect('accounts:home')
     else:
-        signupform = signupForm(tabActive=False)
         loginform = loginForm()
 
-    return render(request, 'registration/login.html', {'signupForm':signupform, 
-        'loginForm':loginform,})
+    return render(request, 'registration/signin.html', {'form':form,})
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        if settings.DEBUG:
+            print(request.POST)
+        form = signupForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login(request,user)
+            return redirect('accounts:home')
+    else:
+        form = signupForm()
+
+    return render(request, 'registration/signup.html', {'form':form})
+
 
